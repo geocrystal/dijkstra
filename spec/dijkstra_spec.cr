feee5613d8f9b2367b2d558c58580f100d909276
Anton Maminov <anton.linux@gmail.com>
2022-01-15 02:31:15 +0200

    add implementation

--- spec/dijkstra_spec.cr
+++ spec/dijkstra_spec.cr
@@ -1,9 +1,123 @@
 require "./spec_helper"
 
-describe Dijkstra do
-  # TODO: Write tests
+describe Dijkstra::Graph do
+  edges = [
+    {'a', 'b', 7},
+    {'a', 'c', 9},
+    {'a', 'f', 14},
+    {'b', 'c', 10},
+    {'b', 'd', 15},
+    {'c', 'd', 11},
+    {'c', 'f', 2},
+    {'d', 'e', 6},
+    {'e', 'f', 9},
+  ]
 
-  it "works" do
-    false.should eq(true)
+  describe "#shortest_path" do
+    it "should find shortests directed path" do
+      gr = Dijkstra::Graph(Char).new(directed: true)
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      gr.shortest_path('a', 'e').should eq({26, ['a', 'c', 'd', 'e']})
+    end
+
+    it "should find shortests undirected path" do
+      gr = Dijkstra::Graph(Char).new
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      gr.shortest_path('a', 'e').should eq({20, ['a', 'c', 'f', 'e']})
+    end
+
+    it "should return nil if no path" do
+      gr = Dijkstra::Graph(Char).new
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      gr.add_edge('x', 'y', 1)
+
+      gr.shortest_path('a', 'x').should eq(nil)
+    end
+
+    it "should raise exception when source node not found" do
+      gr = Dijkstra::Graph(Char).new
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      expect_raises Dijkstra::Error, "Missing source node: 'x'" do
+        gr.shortest_path('x', 'a')
+      end
+    end
+
+    it "should raise exception when target node not found" do
+      gr = Dijkstra::Graph(Char).new
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      expect_raises Dijkstra::Error, "Missing target node: 'x'" do
+        gr.shortest_path('a', 'x')
+      end
+    end
+  end
+
+  describe "#shortest_paths" do
+    it "should find shortests directed paths" do
+      gr = Dijkstra::Graph(Char).new(directed: true)
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      gr.shortest_paths('a').should eq(
+        [
+          {'a', 'b', 7, ['a', 'b']},
+          {'a', 'c', 9, ['a', 'c']},
+          {'a', 'f', 11, ['a', 'c', 'f']},
+          {'a', 'd', 20, ['a', 'c', 'd']},
+          {'a', 'e', 26, ['a', 'c', 'd', 'e']},
+        ]
+      )
+    end
+
+    it "should find shortests undirected paths" do
+      gr = Dijkstra::Graph(Char).new
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      gr.shortest_paths('a').should eq(
+        [
+          {'a', 'b', 7, ['a', 'b']},
+          {'a', 'c', 9, ['a', 'c']},
+          {'a', 'f', 11, ['a', 'c', 'f']},
+          {'a', 'd', 20, ['a', 'c', 'd']},
+          {'a', 'e', 20, ['a', 'c', 'f', 'e']},
+        ]
+      )
+    end
+
+    it "should raise exception when source node not found" do
+      gr = Dijkstra::Graph(Char).new
+
+      edges.each do |source, target, weight|
+        gr.add_edge(source, target, weight)
+      end
+
+      expect_raises Dijkstra::Error, "Missing source node: 'x'" do
+        gr.shortest_paths('x')
+      end
+    end
   end
 end
--- src/dijkstra.cr
+++ src/dijkstra.cr
@@ -1,6 +1,90 @@
-# TODO: Write documentation for `Dijkstra`
 module Dijkstra
-  VERSION = "0.1.0"
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
-  # TODO: Put your code here
+  class Error < Exception
+  end
+
+  class Graph(T)
+    def initialize(@directed = false)
+      # {node1 => {edge1 => weight, edge2 => weight}, node2 => {...}, ...}
+      @neighbours = Hash(T, Hash(T, Int32)).new { |h, k| h[k] = Hash(T, Int32).new }
+
+      @vertices = Set(T).new
+      @infinity = Int32::MAX
+
+      @dist = {} of T => Int32
+      @prev = {} of T => T?
+    end
+
+    def add_edge(source : T, target : T, weight : Int32)
+      @neighbours[source][target] = weight
+      @neighbours[target][source] = weight unless @directed
+
+      @vertices << source << target
+    end
+
+    # Based of wikipedia's pseudocode: https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+    private def dijkstra(source : T)
+      raise Dijkstra::Error.new("Missing source node: '#{source}'") unless @vertices.includes?(source)
+
+      @vertices.each do |i|
+        @dist[i] = @infinity
+        @prev[i] = nil
+      end
+
+      @dist[source] = 0
+      q = @vertices.dup
+
+      while q.any?
+        u = @dist.select(q.to_a).min[0]
+        q.delete(u)
+
+        break if @dist[u] == @infinity
+
+        next unless @neighbours.has_key?(u)
+
+        @neighbours[u].each do |v, weight|
+          alt = @dist[u] + weight
+
+          if alt < @dist[v]
+            @dist[v] = alt
+            @prev[v] = u
+          end
+        end
+      end
+    end
+
+    def shortest_paths(source : T) : Array(Tuple(T, T, Int32?, Array(T)?))
+      dijkstra(source)
+
+      (@vertices - [source]).map do |target|
+        if @dist[target] != @infinity
+          {source, target, @dist[target], path_to(target)}
+        else
+          {source, target, nil, nil}
+        end
+      end
+    end
+
+    def shortest_path(source : T, target : T) : {Int32, Array(T)}?
+      dijkstra(source)
+
+      raise Dijkstra::Error.new("Missing target node: '#{target}'") unless @vertices.includes?(target)
+
+      if @dist[target] != @infinity
+        {@dist[target], path_to(target)}
+      else
+        nil
+      end
+    end
+
+    # Full shortest route to a node
+    private def path_to(target, path = [] of T) : Array(T)
+      if (node = @prev[target])
+        path_to(node, path)
+      end
+
+      path << target
+    end
+  end
 end
